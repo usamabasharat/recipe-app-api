b9bb900ab2d895a5bc3d5689ec87b3e351b3d7e4
Usama Basharat <36359558+usamabasharat@users.noreply.github.com>
2023-04-18 23:54:34 +0500

    Configured postgres and added wait for db command (#1)
    * Configures postgres and added wait for db command
    
    * fixed lint errors
    
    * fixed lint errors
    
    * fixed lint errors

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,24 @@
+import time
+
+from psycopg2 import OperationalError as Psycopg2OpError
+
+from django.db.utils import OperationalError
+from django.core.management import BaseCommand
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        self.stdout.write('Waiting for database...')
+
+        db_up = False
+
+        while db_up is False:
+            try:
+                self.check(databases=['default'])
+                db_up = True
+            except(Psycopg2OpError, OperationalError):
+                self.stdout.write('Database is unavailable, wait for 1 sec!')
+                time.sleep(1)
+
+        self.stdout.write(self.style.SUCCESS('Databse is available!'))
