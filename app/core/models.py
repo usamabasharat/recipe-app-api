157fd692c86f76e90037e47f87cec46e5b451f5e
Usama Basharat <dev.usamabasharat@gmail.com>
2023-04-19 01:16:03 +0500

    Added email validation and superuser

--- app/core/models.py
+++ app/core/models.py
@@ -9,8 +9,18 @@ from django.contrib.auth.models import (
 class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
-        user = self.model(email=email, **extra_fields)
+        if not email:
+            raise ValueError('Email is not provided!')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
+        user.is_superuser = True
+        user.is_staff = True
         user.save(using=self._db)
 
         return user
